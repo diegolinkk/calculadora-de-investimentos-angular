c29e7c7db65e72af213dc75de15e140c8e452afc
Diego Oliveira da Silva <diego-oliveira.silva@serpro.gov.br>
2025-12-23 10:24:40 -0300

    ngModel nos inputs

--- src/app/input-usuarios/input-usuarios.ts
+++ src/app/input-usuarios/input-usuarios.ts
@@ -8,7 +8,12 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './input-usuarios.css',
 })
 export class InputUsuarios {
+  investimentoInicial: number = 0
+  investimentoAnual: number = 0
+  retornoEsperado: number = 0
+  duracao: number = 0
+
   calcular() {
-    console.log("Funcionou")
+    console.log(this.investimentoInicial, this.investimentoAnual, this.retornoEsperado, this.duracao)
   }
 }
