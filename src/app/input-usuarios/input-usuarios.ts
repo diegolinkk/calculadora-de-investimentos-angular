873799018f0179d27ece7ef22cd4504c52dd166c
Diego Oliveira da Silva <diego-oliveira.silva@serpro.gov.br>
2025-12-23 10:08:16 -0300

    formatacao componente input + botao de calcular mockado

--- src/app/input-usuarios/input-usuarios.ts
+++ src/app/input-usuarios/input-usuarios.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-calculadora',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './input-usuarios.html',
   styleUrl: './input-usuarios.css',
 })
 export class InputUsuarios {
-
+  calcular() {
+    console.log("Funcionou")
+  }
 }
